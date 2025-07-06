483001af606c52469e11d80f129dcea894964442
D.S.Aditya <aditya.devendran.s@gmail.com>
2025-07-07 00:23:23 +0530

    basic malloc implemented; works for non aligned too.

--- bootloader/bootloader.c++
+++ bootloader/bootloader.c++
@@ -15,29 +15,14 @@ extern "C" void  __attribute__((noreturn)) call_start_cpu0(void)
    UART::print("Kernel loaded!\r\n");
    UART::print("Compiled on "); UART::print(__DATE__); UART::print(" at "); UART::print(__TIME__); UART::print(".\r\n");
 
-   malloc(10);
+   char* a = (char*)malloc(sizeof(char) * 2);
+   a = "a";
+   bool* y = (bool*)malloc(1);
+   int* x = (int*)malloc(sizeof(int));
+   *x = 19283;
    
-   for(int i = 0; i < 0x2000; i++)//this is first 32kb (0x2000). once we go past, unmapped and corruption occurs? should examine.
-   {
-      unsigned int* p = (unsigned int*)(0x3F800000 + (i * 0x4));
-      *p = (unsigned int)i;
-   }
-
-   UART::print("Writes done\r\n");
-   for(int i = 0; i < 0x2000; i++)
-   {
-      unsigned int* p = (unsigned int*)(0x3F800000 + (i * 0x4));
-
-      if((*p != i) || (i == 0x2000 - 1))
-      {
-         UART::print(i);
-         UART::print(" ");
-         UART::print((unsigned int)*p);
-         UART::print("\r\n");
-      }
-   }
-
-   UART::print("Done!");
+   UART::print(*x);
+   UART::print(a);
    while(1)
    {    
    }
--- libc/string/malloc.cpp
+++ libc/string/malloc.cpp
@@ -9,11 +9,26 @@ extern "C" void* malloc(size_t s)
     unsigned short heapSize = cMMU::getHeapSize();
     unsigned short numEnt = *(unsigned short*)(heapStart + heapSize - 2);
     UART::print("Number of heap entries: "); UART::print(numEnt); UART::print("\r\n");
-
-    for(int i = 0; i < numEnt; i++)
+    
+    for(int i = 1; i <= numEnt; i++)
     {
-        heapMD md = *(heapMD*)(heapStart + heapSize - 2 - ((i + 1) * sizeof(heapMD)));
+        heapMD md = *(heapMD*)(heapStart + heapSize - 2 - (i * sizeof(heapMD)));
         UART::print(md.regAddr);UART::print(" ");UART::print(md.regLength);UART::print("\r\n");
+        if((md.regLength < 0))
+        {
+            if((md.regAddr + s) <= (heapSize - 2 - ((i + 1) * sizeof(heapMD))))//make sure hnew region end wont clash with metadatas new entry
+            {
+                //modify existing md region to account for its shrining and append new region to end.
+                heapMD nmd = {.regAddr = md.regAddr, .regLength = s};
+                md.regAddr = md.regAddr + s;
+                md.regLength = md.regLength + s;
+
+                *(heapMD*)(heapStart + heapSize - 2 - (i * sizeof(heapMD))) = md;//modify current entry to account for its shrinkage
+                *(heapMD*)(heapStart + heapSize - 2 - ((numEnt + 1) * sizeof(heapMD))) = nmd;//append new entry
+                *(unsigned short*)(heapStart + heapSize - 2) = numEnt + 1;//increase number of entries by 1.
+                return((void*)(nmd.regAddr + heapStart));
+            }
+        }
     }
     return(0);
 }
